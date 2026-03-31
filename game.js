fc11e9078dd969bf9d9e40e0ce78bd3f747ee5e4
Patricia <162380840+pattys-code@users.noreply.github.com>
2026-03-31 13:29:53 +0200

    create rock, paper, scissors console game

---
+++ game.js
@@ -0,0 +1,60 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    let choice = Math.floor(Math.random()*3);
+    if (choice===0)
+        {return 'rock';}
+    else if (choice===1)
+        {return 'paper';}
+    else 
+        {return 'scissors';}
+}
+
+function getHumanChoice(){
+    let choice = prompt("Rock, Paper or Scissors?");
+    return choice.toLowerCase();
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice===computerChoice){
+        console.log("It's a tie!");
+        }
+    else if ((humanChoice==='rock' && computerChoice==='paper')
+            || (humanChoice==='paper' && computerChoice==='scissors')
+            || (humanChoice==='scissors' && computerChoice==='rock')){
+        console.log("You lose this round.");
+        computerScore++;
+    }
+    else {
+        console.log("You win this round.");
+        humanScore++;
+    }
+}
+
+function playGame(rounds) {
+    for (let round=1; round<rounds+1;round++) {
+        console.log('ROOUND '+round);
+        const humanChoice = getHumanChoice();
+        const computerChoice= getComputerChoice();
+        console.log('Your choice: '+humanChoice);
+        console.log('Computer choice: '+computerChoice);
+
+        playRound(humanChoice,computerChoice);
+        console.log('Current score (You/Computer): '+humanScore+'/'+computerScore);
+    }
+    console.log('FINAL RESULT:')
+    if (humanScore===computerScore){
+        console.log("It's a tie!");
+    }
+    else if (humanScore>computerScore) {
+        console.log("You win!");
+    }
+    else {
+        console.log("You lose!");
+    }
+}
+
+playGame(5);
+
+
